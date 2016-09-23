8a868fa744512290a53b08e3ea9f0ceb96c9699c
Dan-Cristian Andronic <Cristian@nat-oitwireless-inside-vapornet100-d-1442.Princeton.EDU>
2016-09-23 09:06:20 -0400

    ViewController.swift: Added client code to save Parse objects and send and receive Push notifications

--- ParsePush-Swift-iOS/ParseServerPush/ViewController.swift
+++ ParsePush-Swift-iOS/ParseServerPush/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                print("Successfuly saved object to Parse Server")
+            } else {
+                print("Failed saving object to Parse Server")
+                print(error)
+            }
+        }
+        
+        let parameters = ["channels": ["global"], "message": "Hurray!"]
+        PFCloud.callFunctionInBackground("sendPushToChannels", withParameters: parameters) {
+            (response: AnyObject?, error: NSError?) -> Void in
+            if (error == nil) {
+                print("Successfuly sent Push Notification to Parse Server")
+            } else {
+                print("Failed sending Push Notification to Parse Server")
+                print(error)
+            }
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
