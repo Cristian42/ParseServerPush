3ea413067bcb760246d7a4de3034b4b9f440d503
Dan-Cristian Andronic <Cristian@nat-oitwireless-inside-vapornet100-d-1442.Princeton.EDU>
2016-09-23 08:58:34 -0400

    Added CloudCode to handle Client Push Notifications

--- ParseServer/cloud/main.js
+++ ParseServer/cloud/main.js
@@ -2,3 +2,24 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("sendPushToChannels", function(request, response) {
+
+var channels = request.params.channels;
+var message = request.params.message;
+
+// Send the push notification to the selected channels
+Parse.Push.send({
+  channels: channels,
+  data: {
+    alert: message
+  }
+}, { useMasterKey: true }
+).then(function() {
+    response.success("Push was sent successfully.")
+}, function(error) {
+    response.error("Push failed to send with error: " + error.message);
+});
+
+});
+
